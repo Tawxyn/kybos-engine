e4fdc510dce11383fd12b8bd7249d073556f8555
Tawxyn <oleshchukmike@gmail.com>
2025-01-16 23:01:46 -0800

    Added struct & Verticies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
-}
+fn main() {}
+
+#[derive(Debug, Clone, Copy)]
+struct Matrix([[f32; 4]; 4]);
+
+#[derive(Debug, Clone, Copy)]
+struct Vector([f32; 4]);
+
+const VERTICES: [Vector; 8] = [
+    Vector([-1.0, -1.0, -1.0, 1.0]),
+    Vector([-1.0, -1.0, 1.0, 1.0]),
+    Vector([1.0, -1.0, -1.0, 1.0]),
+    Vector([1.0, -1.0, 1.0, 1.0]),
+    Vector([-1.0, 1.0, -1.0, 1.0]),
+    Vector([-1.0, 1.0, 1.0, 1.0]),
+    Vector([1.0, 1.0, -1.0, 1.0]),
+    Vector([1.0, 1.0, 1.0, 1.0]),
+];
